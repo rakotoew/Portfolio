d30993ff28267e3141f6c2b1a125d08e7974788f
Ewan Rakotoanosy <rakotoew@users.noreply.github.com>
2024-04-10 23:58:27 +0200

    quick change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,20 +8,20 @@ export default function App({Component, pageProps, router}: AppProps) {
     return (
         <>
             <Head>
-                <title>next-pwa-template</title>
+                <title>Ewan Rakotoanosy</title>
 
-                <meta name="subject" content="REPLACE_ME"/>
-                <meta name="description" content="REPLACE_ME"/>
-                <meta name="author" content="REPLACE_ME"/>
+                <meta name="subject" content="Ewan Rakotoanosy"/>
+                <meta name="description" content="Ewan Rakotoanosy Portfolio"/>
+                <meta name="author" content="Ewan Rakotoanosy"/>
                 <meta property="og:type" content="website"/>
-                <meta property="og:url" content="REPLACE_ME"/>
-                <meta property="og:title" content="REPLACE_ME"/>
-                <meta property="og:description" content="REPLACE_ME"/>
+                <meta property="og:url" content="rakotoew.fr"/>
+                <meta property="og:title" content="Ewan Rakotoanosy"/>
+                <meta property="og:description" content="Ewan Rakotoanosy Portfolio"/>
                 <meta property="og:image" content="/assets/banner.png"/>
                 <meta property="twitter:card" content="summary_large_image"/>
-                <meta property="twitter:url" content="REPLACE_ME"/>
-                <meta property="twitter:title" content="REPLACE_ME"/>
-                <meta property="twitter:description" content="REPLACE_ME"/>
+                <meta property="twitter:url" content="rakotoew.fr"/>
+                <meta property="twitter:title" content="Ewan Rakotoanosy"/>
+                <meta property="twitter:description" content="Ewan Rakotoanosy Portfolio"/>
                 <meta property="twitter:image" content="/assets/banner.png"/>
             </Head>
             <NextIntlProvider messages={pageProps.messages}>
