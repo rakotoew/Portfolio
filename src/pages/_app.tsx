6ade725f4bf6856e4a9527902133c98d0b3fb747
rakotoew <55093676+rakotoew@users.noreply.github.com>
2024-05-04 19:41:23 +0200

    change meta header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,22 +9,25 @@ export default function App({Component, pageProps, router}: AppProps) {
         <>
             <Head>
                 <title>Ewan Rakotoanosy</title>
-                <meta name="subject" content="Ewan Rakotoanosy"/>
-                <meta name="description" content="Ewan Rakotoanosy Portfolio"/>
-                <meta name="author" content="Ewan Rakotoanosy"/>
-                <meta property="og:type" content="website"/>
-                <meta property="og:url" content="rakotoew.fr"/>
-                <meta property="og:title" content="Ewan Rakotoanosy"/>
-                <meta property="og:description" content="Ewan Rakotoanosy Portfolio"/>
-                <meta property="og:image" content="/public/icons/icon-512x512.png"/>
-                <meta property="twitter:card" content="summary_large_image"/>
-                <meta property="twitter:url" content="rakotoew.fr"/>
-                <meta property="twitter:title" content="Ewan Rakotoanosy"/>
-                <meta property="twitter:description" content="Ewan Rakotoanosy Portfolio"/>
-                <meta property="twitter:image" content="/public/icons/icon-512x512.png"/>
+                <meta name="subject" content="Ewan Rakotoanosy" />
+                <meta name="description" content="Ewan Rakotoanosy Portfolio" />
+                <meta name="author" content="Ewan Rakotoanosy" />
+                <meta property="og:type" content="portfolio" />
+                <meta property="og:url" content="rakotoew.fr" />
+                <meta property="og:title" content="Ewan Rakotoanosy" />
+                <meta property="og:description" content="Ewan Rakotoanosy Portfolio" />
+                <meta property="og:image" content="/public/profile.png" />
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="rakotoew.fr" />
+                <meta property="twitter:title" content="Ewan Rakotoanosy" />
+                <meta property="twitter:description" content="Ewan Rakotoanosy Portfolio" />
+                <meta property="twitter:image" content="/public/profile.png" />
+                <meta name="viewport" content="width=device-width,initial-scale=1" />
+                <link rel="canonical" href="https://rakotoew.fr" />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
             <NextIntlProvider messages={pageProps.messages}>
-                <Component {...pageProps} key={router.route}/>
+            <Component {...pageProps} key={router.route}/>
             </NextIntlProvider>
         </>
     );
