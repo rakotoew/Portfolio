baaef525aba32047036a0ccb87b2b45cafe1743d
rakotoew <55093676+rakotoew@users.noreply.github.com>
2024-05-04 23:29:51 +0200

    footer lang

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,17 @@
 import styles from "@styles/components/footer.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { useTranslations } from "use-intl";
 export default function Footer() {
+    const t = useTranslations("footer");
     return (
         <>
             <footer className={styles.footer_container}>
-                <p></p>
+                <p className={styles.filler}></p>
                 <p>© 2024 Ewan Rakotoanosy</p>
                 <div className={styles.lang_button}>
                     <FontAwesomeIcon className={styles.icon} icon={faGlobe} size="2x"/>
+                    <p className={styles.lang}>{t("language")}</p>
                 </div>
             </footer>
         </>
