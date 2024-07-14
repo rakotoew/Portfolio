d112c1643ea3b1542115a74d4d406108f6c3026d
Ewan Rakotoanosy <rakotoew@users.noreply.github.com>
2024-07-14 15:27:51 +0200

    change language

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,18 @@ import styles from "@styles/components/footer.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { useTranslations } from "use-intl";
+import { useRouter } from "next/router";
+import Link from "next/link";
 export default function Footer() {
     const t = useTranslations("footer");
+    const router = useRouter();
 
     const changeLanguage = () => {
-        
+        if(router.locale === "en"){
+            router.push(router.asPath, router.asPath, {locale: "fr"});
+        }else {
+            router.push(router.asPath, router.asPath, {locale: "en"});
+        }
     };
     return (
         <footer className={styles.footer_container}>
@@ -14,7 +21,7 @@ export default function Footer() {
             <p>© 2024 Ewan Rakotoanosy</p>
             <div onClick={changeLanguage} className={styles.lang_button}>
                 <FontAwesomeIcon className={styles.icon} icon={faGlobe} size="2x"/>
-                <p className={styles.lang}>{t("language")}</p>
+                <p className={styles.lang} >{t("language")}</p>
             </div>
         </footer>
     );
