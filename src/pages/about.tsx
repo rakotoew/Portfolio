c829f124de562fb350f82e9c276e0e6450edea42
Ewan Rakotoanosy <rakotoew@users.noreply.github.com>
2024-08-01 01:20:15 +0200

    ajout page exp and about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,12 @@
 import { GetStaticProps } from "next";
 import { useTranslations } from "use-intl";
-
+import styles from "@styles/pages/about.module.css";
 function About(): JSX.Element {
     const t = useTranslations("about");
     return (
-        <div>
+        <div className={styles.about_container}>
+            <h1 className={styles.title}>{t("name")}</h1>
+            <p className={styles.text}>{t("text")}</p>
         </div>
     );
 }
