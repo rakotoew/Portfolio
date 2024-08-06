ca3976473976dc71c89571d7b5b0e9aac7d542f5
Ewan Rakotoanosy <rakotoew@users.noreply.github.com>
2024-08-06 10:58:55 +0200

    home page part 1

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,24 @@
 import { GetStaticProps } from "next";
 import { useTranslations } from "use-intl";
 import styles from "@styles/pages/about.module.css";
+import Image from "next/image";
 function About(): JSX.Element {
     const t = useTranslations("about");
     return (
         <div className={styles.about_container}>
             <h1 className={styles.title}>{t("name")}</h1>
-            <p className={styles.text}>{t("text")}</p>
+            <div className={styles.left_container}>
+                <Image className={styles.image_pro} src="/professionnal.jpg" alt='Ewan Rakotoanosy' width={750} height={1000} />
+                <p className={styles.text_right}>{t("text1")}</p>
+            </div>
+            <div className={styles.right_container}>
+                <Image className={styles.image_ireland} src="/profile.png" alt='Ewan Rakotoanosy' width={750} height={750} />
+                <p className={styles.text_left}>{t("text2")}</p>
+            </div>
+            <div className={styles.left_container}>
+                <Image className={styles.image_vtt} src="/vttCroixdeFrix.PNG" alt='Ewan Rakotoanosy' width={750} height={500} />
+                <p className={styles.text_right}>{t("text3")}</p>
+            </div>
         </div>
     );
 }
