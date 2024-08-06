9849d71b1f9a7be604656ee5d10b785dab322576
Ewan Rakotoanosy <rakotoew@users.noreply.github.com>
2024-08-06 14:53:34 +0200

    french content experiences

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -12,20 +12,21 @@ function Experience(): JSX.Element{
                 <div className={styles.experience_list}>
                     <ExperienceCard path="experiences.exp1" />
                     <ExperienceCard path="experiences.exp2" />
+                    <ExperienceCard path="experiences.exp3" />
                 </div>
             </div>
             <div className={styles.list_container}>
                 <p className={styles.list_title}>{t("personal")}</p>
                 <div className={styles.experience_list}>
-                    <ExperienceCard path="experiences.exp1" />
-                    <ExperienceCard path="experiences.exp2" />
+                    <ExperienceCard path="experiences.exp4" />
                 </div>
             </div>
             <div className={styles.list_container}>
                 <p className={styles.list_title}>{t("univ")}</p>
                 <div className={styles.experience_list}>
-                    <ExperienceCard path="experiences.exp1" />
-                    <ExperienceCard path="experiences.exp2" />
+                    <ExperienceCard path="experiences.exp5" />
+                    <ExperienceCard path="experiences.exp6" />
+                    <ExperienceCard path="experiences.exp7" />
                 </div>
             </div>
         </div>
